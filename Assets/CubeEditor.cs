8194b496013bd995f21a3f831abb768d8bc92c10
TylerDevers <devers.tyler@gmail.com>
2018-02-02 12:04:01 -0800

    added EnemyMovement.cs and Block.cs

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -10,21 +10,24 @@ public class CubeEditor : MonoBehaviour {
     TextMesh textMesh;
 
 	[SerializeField] [Range(1f, 20f)] int gridSize = 10;
-
+    
     void Start()
     {
 
     }
     void Update()
     {
+        string labelText;
         Vector3 snapPos;
+
 		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
 		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
 		transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
-
         textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
 
     }
 
