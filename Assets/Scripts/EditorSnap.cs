e319f1c6faf169d7dae5eeb2a1541e239636dc06
TylerDevers <devers.tyler@gmail.com>
2018-02-27 12:02:24 -0800

    added EditorSnap.cs, cubes now snap to grid in editor

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+	[SerializeField] [Range(10f, 20f)] int gridSize = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 snapPos;
+		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+		transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+	}
+}
