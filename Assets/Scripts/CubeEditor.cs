c21f0dfc2ef121a633f9cc05a21dcdc3103510cc
TylerDevers <devers.tyler@gmail.com>
2018-02-28 10:30:02 -0800

    added auto-filling labels to top of cube

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
 	[SerializeField] [Range(10f, 20f)] int gridSize = 10;
+
+	TextMesh textMesh;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -16,7 +20,9 @@ public class EditorSnap : MonoBehaviour {
 		Vector3 snapPos;
 		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
 		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
 		transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+
+		textMesh = GetComponentInChildren<TextMesh>();
+		textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize ;	
 	}
 }
