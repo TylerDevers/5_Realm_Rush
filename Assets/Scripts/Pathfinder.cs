66ebc11948293f85512a13947f0a63dcafe2c280
TylerDevers <devers.tyler@gmail.com>
2018-03-05 11:54:55 -0800

    begun Neighbor exploration

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -7,23 +7,51 @@ public class Pathfinder : MonoBehaviour {
 	[SerializeField] Waypoint startWaypoint, endWaypoint;
 
 	Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+	Vector2Int[] directions = {
+		Vector2Int.up,
+		Vector2Int.right,
+		Vector2Int.down,
+		Vector2Int.left
+	};
 
 	// Use this for initialization
 	void Start () {
 		LoadBlocks();
 		ColorStartAndEnd();
+		ExploreNeighbors();
 	}
-
-    void ColorStartAndEnd()
-    {
-        startWaypoint.SetTopColor(Color.blue);
-		endWaypoint.SetTopColor(Color.red);
-    }
 
     // Update is called once per frame
     void Update () {
 		
 	}
+
+	void ExploreNeighbors()
+	{
+		Vector2Int startBlock = startWaypoint.GetGridPos();
+		
+		foreach (Vector2Int direction in directions)
+		{
+			Vector2Int explorationCoordinates = startBlock + direction;
+			try
+			{
+				// change color of neighbor
+				grid[explorationCoordinates].SetTopColor(Color.blue);
+
+			}
+			catch
+			{
+				print(explorationCoordinates + " Not Found");
+			}
+		}
+	}
+    void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.yellow);
+		endWaypoint.SetTopColor(Color.red);
+    }
+
+
     private void LoadBlocks()
     {
         var waypoints = FindObjectsOfType<Waypoint>();
