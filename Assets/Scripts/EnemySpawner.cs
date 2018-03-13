0726499e483bdc2038024cdad2fb4c0440ae8a11
TylerDevers <devers.tyler@gmail.com>
2018-03-13 11:00:09 -0700

    enemySpawner.cs added, towers now target new closest enemies upon enemy death.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
-	[SerializeField] float secondsBetweenSpawn = 3f;
-	[SerializeField] private GameObject enemy;
-	private int enemyCount = 0;
+	[SerializeField] [Range(0.1f, 120f)] float secondsBetweenSpawn = 3f;
+	[SerializeField] private GameObject enemySpawn;
 
 	// Use this for initialization
 	void Start () {
@@ -19,11 +18,10 @@ public class EnemySpawner : MonoBehaviour {
 
 	IEnumerator EnemySpawn() 
 	{
-		while(enemyCount < 5)
+		while(true)
 		{
-
-			Instantiate(enemy, transform.position, Quaternion.identity);
-			enemyCount ++;
+			GameObject enemy = Instantiate(enemySpawn, transform.position, Quaternion.identity);
+			enemy.transform.parent = gameObject.transform;
 			//print("enemy number " + enemyCount);
 			yield return new WaitForSeconds(secondsBetweenSpawn);
 		}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour {
 
+    //Parameters of objects
     [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targetEnemy;
     [SerializeField] float attackRange = 10f;
     [SerializeField] ParticleSystem projectileParticle;
 	
+    //States of objects
+    Transform targetEnemy;
 	// Update is called once per frame
 	void Update () {
+        SetTargetEnemy();
         if (targetEnemy)
         {
             objectToPan.LookAt(targetEnemy);
@@ -22,6 +25,30 @@ public class Tower : MonoBehaviour {
             Shoot(false);
         }
 	}
+
+    private void SetTargetEnemy()
+    {
+        var sceneEnemies = FindObjectsOfType<EnemyDamage>(); //load all enemies in an array
+        if (sceneEnemies.Length == 0 ) { return; } //exits if no enemies in list
+        Transform closestEnemy = sceneEnemies[0].transform; //assumes first enemy in array is the closest
+        foreach (EnemyDamage testEnemy in sceneEnemies) // iterate over array, assign the closest enemy to a variable
+        {
+            closestEnemy = GetClosest(closestEnemy, testEnemy.transform);
+        }
+        targetEnemy = closestEnemy; //assign the closest enemy as the target enemy
+
+    }
+
+    private Transform GetClosest(Transform transformA, Transform transformB)
+    {
+        var distToA = Vector3.Distance(transform.position, transformA.position);
+        var distToB = Vector3.Distance(transform.position, transformB.position);
+        if (distToA < distToB)
+        {
+            return transformA;
+        }
+        return transformB;
+    }
 
     private void FireAtEnemy()
     {
