d69aae02baa8eb49f0fcf4995a02a87bbab04d98
TylerDevers <devers.tyler@gmail.com>
2018-03-13 11:38:30 -0700

    redesigned blocks to work with OnMouseOver()

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -24,7 +24,7 @@ public class EnemyDamage : MonoBehaviour {
     void ProcessHit()
     {
         hitPoints --;
-        print("current hitpoints are " + hitPoints);
+        //print("current hitpoints are " + hitPoints);
     }
 
     private void KillEnemy()
