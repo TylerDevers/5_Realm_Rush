ad2eaf55eef5ff64f7ae510cbcd6ec8a22ecc1df
TylerDevers <devers.tyler@gmail.com>
2018-03-28 10:29:24 -0700

    commiting final changes. game unfinished @ 149

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -7,6 +7,8 @@ public class EnemyDamage : MonoBehaviour {
 
     [SerializeField] Collider collisionMesh;
     [SerializeField] int hitPoints = 5;
+    [SerializeField] ParticleSystem hitParticlePrefab;
+    [SerializeField] ParticleSystem hitDeathParticlePrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -24,11 +26,14 @@ public class EnemyDamage : MonoBehaviour {
     void ProcessHit()
     {
         hitPoints --;
+        hitParticlePrefab.Play();
         //print("current hitpoints are " + hitPoints);
     }
 
     private void KillEnemy()
     {
+        var vfx = Instantiate(hitDeathParticlePrefab, transform.position, Quaternion.identity);
+        vfx.Play();
         Destroy(gameObject);
     }
 }
